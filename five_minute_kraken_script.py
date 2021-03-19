a940173c36fc8730d852554624f04bd7e6b01894
MattN <mattnewkirk555@gmail.com>
2021-03-18 18:45:45 -0700

    Added btc_articles.csv

--- five_minute_kraken_script.py
+++ five_minute_kraken_script.py
@@ -38,13 +38,16 @@ async def tweet_search_sent(string_query, number_tweets = 500):
     df = pd.DataFrame()
     spinner = spinning_cursor()
     for tweets in tweepy.Cursor(api.search, q=string_query).items(number_tweets):
+        
+        ###Spinning Progress Bar
         sys.stdout.write(next(spinner))
         sys.stdout.flush()
         time.sleep(0.1)
         sys.stdout.write('\b')
+        ### /Spinning Progress Bar
+        
         contents = tweets.text
         sentiment.append(contents)
-        #sentiment.append(sentiment_reader_comp(contents))
     df = pd.DataFrame(sentiment, columns=['text'])
     df['text'] = df['text'].apply(sentiment_reader_comp)
     return df['text'].mean()
@@ -72,9 +75,6 @@ async def get_redd_sent(subreddit):
     return df_response['sent'].mean()
 
 
-# In[9]:
-
-
 async def get_kraken(ticker = "BTC/USD"):
     data = {}
     load_dotenv()
@@ -88,16 +88,20 @@ async def get_kraken(ticker = "BTC/USD"):
 
 
 async def main(loop):
+    #Initialize Important Variables
     x = 0
-    
     global df
     global dict_btc
     df = pd.DataFrame()
     loops = 500
     avg_time = 0
+    
+    #Main Script Loop Starts Here
     while x <= loops:
         wait = 300 #seconds
-        start = time.time()
+        start = time.time() #Start our stopwatch for benchmarking purposes.
+        
+        #Call the Kraken! The Kraken API method returns a dictionary object with our ticker data.
         try:
             print('Collecting Ticker Data')
             dict_btc = await get_kraken("BTC/USD")
@@ -105,8 +109,8 @@ async def main(loop):
         except:
             dict_btc = {}
             print(f"Kraken fail at {x}")
-        #await asyncio.sleep(2)
         
+        #Gather and Analyze Tweets
         try:
             print("Gathering Tweets")
             twi_sent = await tweet_search_sent('bitcoin OR btc')
@@ -114,7 +118,8 @@ async def main(loop):
         except:
             twi_sent = 0
             print(f"Tweet fail at {x}")
-        #await asyncio.sleep(2)
+        
+        #Read and Analyze Reddit
         try:
             print("Reading Reddit")
             redd_sent = await get_redd_sent('bitcoin')
@@ -122,23 +127,29 @@ async def main(loop):
         except:
             redd_sent = 0
             print(f"Reddit fail at {x}")
-        #await asyncio.sleep(2)
+        #Delete Extraneous information from our ticker dictionary object.
+        #'info' is a dictionary key pointing to a nested dictionary.
         try:
             del dict_btc['info']
         except:
             print("Empty Dict")
         
+        #Add our Sentiment Analysis columns. Append the dictionary object to our dataframe. Increment our loop counter x.
         dict_btc['twi_sent'] = twi_sent
         dict_btc['redd_sent'] = redd_sent
         df = df.append(dict_btc, ignore_index=True)
         x += 1
         
+        #Printing Info from the Loop
         print(f"DataFrame Shape: {df.shape}")
         end = time.time()
         runtime = end - start ###https://www.blog.pythonlibrary.org/2016/05/24/python-101-an-intro-to-benchmarking-your-code/
         msg = "The runtime for this loop took {time} seconds to complete"
         print(msg.format(time=runtime))
         avg_time += runtime
+        
+        #Subtract the loop runtime from the wait time such that our data points are more evenly distributed.
+        #And Print a countdown clock
         wait -= int(round(runtime,0))
         print("Remaining Wait: (ctrl+c to break) ")
         for i in range(wait): ###https://stackoverflow.com/questions/25189554/countdown-clock-0105/50148334
@@ -147,10 +158,13 @@ async def main(loop):
             #timeformat = ':{:02d}'.format(wait-i)
             print(timeformat, end='\r')
             await asyncio.sleep(1)
+            
+    #Finally, calculate average loop runtime.
     avg_time = avg_time / x
     msg = "The average runtime for each loop was {time} seconds to complete"
     print(msg.format(time=avg_time))
 
+#Call our main function.
 loop = ""
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
